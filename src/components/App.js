fb4c8a309760f814cfa2894f47099e2642c25cc3
Igor Boiko <igorboiko@gmail.com>
2023-04-27 17:33:29 +0300

    add components

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,13 @@
 import { Layout } from './Layout/Layout';
 import { GlobalStyle } from './GlobalStyle';
 import { Component } from 'react';
+import { Searchbar } from './Searchbar/Searchbar';
 
 export class App extends Component {
   render() {
     return (
       <Layout>
-        Render
+        <Searchbar />
         <GlobalStyle />
       </Layout>
     );
